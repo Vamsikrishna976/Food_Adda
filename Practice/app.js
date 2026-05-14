0996a9e99a0354ce13c19b88c710023c83f935dd
VamsiKrishna976 <paluruvamsikrishna16@gmail.com>
2026-05-15 02:31:12 +0530

    Refactor app.js to use JSX syntax for rendering and update package.json to include start and build scripts. Modify Practice/app.js to enhance structure and readability of React elements.

--- Practice/app.js
+++ Practice/app.js
@@ -9,21 +9,25 @@
 //     </div>
 // 
 
- const parent = React.createElement(
-    "div", {id : "parent"}, 
-     React.createElement("div", {}, 
-        React.createElement("div", {}, 
-            [React.createElement("h1", { id : "h1"}, ("This is h1")),
-            React.createElement("h2", { id : "h2"}, "This is h2")])
-        )
-     );
+ 
 
-     const root = ReactDOM.createRoot(document.getElementById("root"));
+let heading;
+heading = React.createElement("div",{},
+    React.createElement("div",{},
+        [React.createElement("div",{},
+            [React.createElement("h1", {}, "This is h1"),
+                React.createElement("h2", {}, "This is h2")]
+        ),
+        React.createElement("div",{},
+            [React.createElement("h1", {}, "This is h1"),
+                React.createElement("h2", {}, "This is h2")]
+        )]
+    )
+);
 
-     root.render(parent)
 
-    // const heading = React.createElement("h1", {}, "This is heading");
 
-    // const root = ReactDOM.createRoot(document.getElementById("root"));
 
-    // root.render(heading);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading)
